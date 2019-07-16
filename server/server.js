8104cbd8f53c57bea64b91293e2d4fa341440414
Hüseyin ATEŞ <huseyinq@mobiliz.com.tr>
2019-07-16 15:33:43 +0300

    baslangic projesi

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+const app = express();
+
+
+app.use(express.static('public'));
+
+
+
+app.listen(3000, (err) => {
+    if (err) {
+        throw err;
+    }
+
+    console.log('Server started!');
+});
